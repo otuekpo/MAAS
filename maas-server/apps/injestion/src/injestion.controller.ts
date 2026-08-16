369d809563650e964f9b8b92e2a54aa629f75de1
Godwins Godwin <godwinsudoinyang@gmail.com>
2026-08-16 13:34:56 +0100

    secuity

--- maas-server/apps/injestion/src/injestion.controller.ts
+++ maas-server/apps/injestion/src/injestion.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, HttpCode, Post } from "@nestjs/common";
+import { BadRequestException, Body, Controller, HttpCode, Post, Req } from "@nestjs/common";
 import { InjestionService } from "./injestion.service";
 import { IngestSensorDataDto } from "./dto/ingest-sensor-data.dto";
 import { Throttle } from "@nestjs/throttler";
+import type {Request} from "express"
 
 @Controller("ingest")
 export class InjestionController {
@@ -10,7 +11,12 @@ export class InjestionController {
   @Post()
   @Throttle({ default: { limit: 60, ttl: 60_000 } })
   @HttpCode(202)
-  ingest(@Body() dto: IngestSensorDataDto) {
+  ingest(@Body() dto: IngestSensorDataDto, @Req() req: Request) {
+    const x_api_key = req.headers["x-api-key"]
+    console.log(x_api_key);
+    if(!x_api_key){
+      throw new BadRequestException("You are not allowed to access this endpoint")
+    }
     return this.injestionService.ingest(dto);
   }
 }
